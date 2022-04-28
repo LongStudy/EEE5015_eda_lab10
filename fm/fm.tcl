358a8d1c8c4e0f277d0e7bdd66ad4e5a0538a17b
xzy <xzyxzy0224@outlook.com>
2022-04-28 17:18:47 +0800

    fix

--- fm/fm.tcl
+++ fm/fm.tcl
@@ -9,10 +9,9 @@
 ########################################################################
 # TCL script for formality                                             #
 ########################################################################
-set designer "Elvis"
-set company  "SUS TECH"
+set designer "XXXXX"
+set company  "SUSTECH"
 
-#set proj_path     /mnt/hgfs/win_linux-2/2019/Work/proj_full_adder		
 set proj_path     ../
 set work_path     $proj_path/fm
 set rtl_path      $proj_path/rtl
@@ -20,14 +19,14 @@ set ntl_path      $proj_path/dc/outputs
 set lib_path      $proj_path/lib
 set rpt_path      $proj_path/fm/reports
 set search_path   "$search_path $work_path $rtl_path $lib_path"
-set design_name   full_adder
+set design_name   multiplier
 
 read_verilog -c r -l WORK -01 "$rtl_path/$design_name.v"
 set hdlin_warn_on_mismatch_message FMR_ELAB-147
 set_top r:/WORK/$design_name
 
 read_verilog -c i -l WORK "$ntl_path/$design_name.v"
-read_db "$lib_path/typical.db"
+read_db "$lib_path/sc_max.db"
 set_top i:/WORK/$design_name
 
 set verification_failing_point_limit 100
